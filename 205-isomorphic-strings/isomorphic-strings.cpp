bd2fc5fc887c4b86f7a8c524d9d7a8dc8227c01c
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-04-04 03:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (98.64%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> m1(128, -1); 
+        vector<int> m2(128, -1); 
+
+        for (int i = 0; i < s.size(); i++) {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if (m1[c1] != -1 && m1[c1] != c2) return false;
+            if (m2[c2] != -1 && m2[c2] != c1) return false;
+
+            m1[c1] = c2;
+            m2[c2] = c1;
+        }
+
+        return true;
+    }
+};
