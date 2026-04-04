487caf33ba7441c1f89c21d02936fe2c52d8156b
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-04-04 22:33:22 +0530

    Time: 1 ms (55.09%) | Memory: 9.9 MB (10.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,15 +3,15 @@ public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length()) return false;
 
-        vector<int> freq(26, 0);
+        unordered_map<char, int> freq;
 
         for (int i = 0; i < s.length(); i++) {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
+            freq[s[i]]++;
+            freq[t[i]]--;
         }
 
-        for (int count : freq) {
-            if (count != 0) return false;
+        for (auto &pair : freq) {
+            if (pair.second != 0) return false;
         }
 
         return true;
