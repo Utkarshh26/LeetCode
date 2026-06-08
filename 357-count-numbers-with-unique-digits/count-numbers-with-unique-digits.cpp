00770ec8c51c0882544ed4356695b4df83af44c1
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-06-09 03:45:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.41%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
         if (n == 0) return 1;
-        int ans = 10;      
-        int unique = 9;    
+
+        vector<int> dp(n + 1);
+        dp[1] = 9;
         int available = 9;
 
         for (int i = 2; i <= n; i++) {
-            unique *= available;
-            ans += unique;
+            dp[i] = dp[i - 1] * available;
             available--;
+        }
+
+        int ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans += dp[i];
         }
 
         return ans;
