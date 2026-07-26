6dbe21416ed285874e9cb7897251c4b89aa9cfbe
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-07-26 20:21:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (16.24%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.cpp
+++ 1542-consecutive-characters/consecutive-characters.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maxPower(string s) {
-        stack<char> st;
-        int maxpower = INT_MIN, power =1;
-        for(char ch: s){
-            if(!st.empty() && ch == st.top()){
+        int maxpower = 1, power = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == s[i - 1]) {
                 power++;
-            }else{
-                maxpower = max(maxpower,power);
-                power= 1;
+            } else {
+                maxpower = max(maxpower, power);
+                power = 1;
             }
-            st.push(ch);
         }
-        maxpower= max(maxpower, power);
-        return maxpower;
+        return max(maxpower, power);
     }
 };
