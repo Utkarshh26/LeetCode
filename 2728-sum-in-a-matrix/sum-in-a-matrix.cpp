affa9179c07eaff29cb31fbd84f665231cb6aaaa
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-08-30 02:50:13 +0530

    Time: 67 ms (8.23%) | Memory: 62.2 MB (7.38%) - LeetSync

--- 2728-sum-in-a-matrix/sum-in-a-matrix.cpp
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int matrixSum(vector<vector<int>>& nums) {
-        int ans = 0;
-        for (auto &row : nums) {
-            sort(row.begin(), row.end());
-        }
+        int n = nums.size(),m = nums[0].size();
+        vector<priority_queue<int>> pq(n);
         
-        for (int col = 0; col < nums[0].size(); col++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                pq[i].push(nums[i][j]);
+            }
+        }
+
+        int ans = 0;
+        for(int j = 0; j < m; j++) {
             int maxi = 0;
-            for (int row = 0; row < nums.size(); row++) {
-                maxi = max(maxi, nums[row][col]);
+            for(int i = 0; i < n; i++) {
+                maxi = max(maxi, pq[i].top());
             }
             ans += maxi;
+            for(int i = 0; i < n; i++) {
+                pq[i].pop();
+            }
         }
         return ans;
     }
