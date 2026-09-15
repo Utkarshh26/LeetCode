f9aa0dd80f340911afc1095c317f7d0963c2db81
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-09-16 03:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (88.59%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    vector<bool> visited;
+private:
+    bool helper(vector<int>&arr, int idx) {
+        if (arr[idx] ==0){
+            return true;
+        }
+        if (visited[idx]){
+            return false;
+        }
+        visited[idx]= true;
+
+        if (idx+ arr[idx]< arr.size()){
+            if (helper(arr, idx+ arr[idx])){
+                return true;
+            }
+        }
+        if (idx -arr[idx] >=0){
+            if (helper(arr,idx -arr[idx])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+public:
+    bool canReach(vector<int>& arr, int start) {
+        visited = vector<bool>(arr.size(),false);
+        return helper(arr,start);
+    }
+};
