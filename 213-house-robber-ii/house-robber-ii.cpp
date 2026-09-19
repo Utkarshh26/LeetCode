811288898d0c54a23bb613055e3b145740e5cf0c
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-09-19 07:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (61.46%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,31 +1,28 @@
 class Solution {
-    vector<int> dp;
-private:
-    int helper(vector<int>& nums,int idx, int n){
-        if(idx >= n){
-            return 0;
-        }
-        if(dp[idx]!= -1){
-            return dp[idx];
-        }
-        int steal= nums[idx]+helper(nums, idx+2,n);
-        int skip = helper(nums, idx+1,n); 
-        return dp[idx]=max(steal,skip);
-    }
 public:
     int rob(vector<int>& nums) {
-        int n= nums.size();
-        if(n ==1){
+        int n = nums.size();
+        if (n == 1){
             return nums[0];
         }
-        if(n==2){
-            return max(nums[0],nums[1]);
+        vector<int> dp(n+ 1, 0);
+        dp[0]= 0; dp[1] =nums[0];
+
+        for (int i= 2; i< n;i++) {
+            int steal= nums[i- 1]+ dp[i -2];
+            int skip =dp[i - 1];
+            dp[i] =max(steal, skip);
         }
-        
-        dp.assign(n,-1);
-        int firststeal =helper(nums,0, n-1);
-        dp.assign(n,-1);
-        int firstskipped= helper(nums,1,n);
-        return max(firststeal,firstskipped);
+        int steal0 = dp[n- 1];
+
+        dp.assign(n +1,0);
+        dp[0] = 0; dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            int steal = nums[i - 1] + dp[i - 2];
+            int skip = dp[i - 1];
+            dp[i] = max(steal, skip);
+        }
+        int skip0 = dp[n];
+        return max(steal0, skip0);
     }
 };
