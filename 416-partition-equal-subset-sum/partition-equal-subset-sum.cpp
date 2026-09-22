9c6ceb192250045953966bbc4555671e9ac17d12
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-09-23 02:53:50 +0530

    Time: 128 ms (58.02%) | Memory: 110.6 MB (36.13%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
     vector<vector<int>>dp;
-    bool helper(int idx,int tar,vector<int>&nums){
+    bool helper(int idx,int tar,vector<int>&arr){
         if(tar ==0){
             return true;
         }
-        if(idx ==nums.size()-1){
-            return nums[nums.size()-1] ==tar;
+        if(idx ==0){
+            return arr[0] ==tar;
         }
         if(dp[idx][tar] !=-1){
             return dp[idx][tar];
         }
-        bool notpick =helper(idx+1,tar,nums);
+        bool notpick =helper(idx-1,tar,arr);
         bool pick =false;
-        if(tar >= nums[idx]){
-            pick =helper(idx+1,tar-nums[idx],nums);
+        if(tar >= arr[idx]){
+            pick =helper(idx-1,tar-arr[idx],arr);
         }
         return dp[idx][tar]= notpick || pick;
     }
@@ -26,8 +26,8 @@ public:
         if(sum%2){
             return false;
         }
-        int tar = sum/2;
-        dp.assign(nums.size(),vector<int>(tar+1,-1));
-        return helper(0,tar,nums);
+        int tar = sum/2, n =nums.size();
+        dp.assign(n,vector<int>(tar+1,-1));
+        return helper(n-1,tar,nums);
     }
 };
