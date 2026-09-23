dfee9ef022a048835f5b1b4e16d1fd85cabdf689
Utkarsh Chaudhary <97533612+Utkarshh26@users.noreply.github.com>
2026-09-23 19:03:50 +0530

    Time: 52 ms (22.29%) | Memory: 48.8 MB (40.96%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,28 @@
 class Solution {
-    int helper(int idx, int tar, vector<vector<int>>&dp,vector<int>&coins){
-        if(idx ==0){
-            if(tar%coins[0] ==0){
-                return tar/coins[0];
-            }
-            return 1e9;
-        }
-        if(dp[idx][tar] !=-1){
-            return dp[idx][tar];
-        }
-        int notpick =helper(idx-1,tar,dp,coins);
-        int pick = 1e9;
-        if(coins[idx] <=tar){
-            pick =1 +helper(idx,tar-coins[idx],dp,coins);
-        }
-        return dp[idx][tar] =min(notpick,pick);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        int ans = helper(n-1,amount,dp,coins);
-        return ans >= 1e9 ?-1 :ans;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        for(int i = 0; i <= amount; i++) {
+            if(i % coins[0] == 0){
+                 dp[0][i] = i/coins[0];
+            }else{
+                dp[0][i] = 1e9;
+            }        
+        }
+
+        for(int idx = 1; idx < n; idx++) {
+            for(int tar = 0; tar <= amount; tar++) {
+                int notpick = dp[idx - 1][tar];
+                int pick = 1e9;
+
+                if(coins[idx] <= tar) {
+                    pick = 1 + dp[idx][tar - coins[idx]];
+                }
+                dp[idx][tar] = min(pick, notpick);
+            }
+        }
+        int ans = dp[n - 1][amount];
+        return ans >= 1e9 ? -1 : ans;
     }
 };
